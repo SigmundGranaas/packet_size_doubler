f520db38b3ef3f345f1465204b623fc1ed6e1c07
Sigmund Granaas <sigmundsgranaas@gmail.com>
2022-05-21 19:42:37 +0200

    Added an entrypoint

---
+++ src/main/java/com/sigmundgranaas/packetsizedoubler/PacketDoublerMod.java
@@ -0,0 +1,10 @@
+package com.sigmundgranaas.packetsizedoubler;
+
+import net.fabricmc.api.ModInitializer;
+
+public class PacketDoublerMod implements ModInitializer {
+    @Override
+    public void onInitialize() {
+
+    }
+}
